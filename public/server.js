498c5776ec5606a97894cdc45237833217800cbf
andrelbdutra <bazulylbd@gmail.com>
2023-10-09 17:25:45 -0300

    testando deploy

--- public/server.js
+++ public/server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 app.use(cors());
 
 // Certificado ssl (não válidado)
-const openSSL = {
-  key: fs.readFileSync('./ssl/key.pem'),
-  cert: fs.readFileSync('./ssl/cert.pem')
-};
+// const openSSL = {
+//   key: fs.readFileSync('./ssl/key.pem'),
+//   cert: fs.readFileSync('./ssl/cert.pem')
+// };
 
 // Configurar o Express para servir arquivos estáticos na pasta "public"
 app.use(express.static('public'));
@@ -30,22 +30,24 @@ app.post('/upload', (req, res) => {
     res.json(processResult);
 });
 
-const server = https.createServer(openSSL, app);
-server.listen(port, () => {
-    const os = require('os');
-    const networkInterfaces = os.networkInterfaces();
-    let ipAddress;
+app.listen(process.env.PORT || 3000)
 
-    // Encontre o endereço IP da máquina local (geralmente o primeiro na lista)
-    for (const interfaceName in networkInterfaces) {
-      const networkInterface = networkInterfaces[interfaceName];
-      for (const interfaceDetails of networkInterface) {
-        if (interfaceDetails.family === 'IPv4' && !interfaceDetails.internal) {
-          ipAddress = interfaceDetails.address;
-          break;
-        }
-      }
-      if (ipAddress) break;
-    }
-    console.log(`Servidor rodando em https://${ipAddress}:${port}`);
-});
+// const server = https.createServer(openSSL, app);
+// server.listen(port, () => {
+//     const os = require('os');
+//     const networkInterfaces = os.networkInterfaces();
+//     let ipAddress;
+
+//     // Encontre o endereço IP da máquina local (geralmente o primeiro na lista)
+//     for (const interfaceName in networkInterfaces) {
+//       const networkInterface = networkInterfaces[interfaceName];
+//       for (const interfaceDetails of networkInterface) {
+//         if (interfaceDetails.family === 'IPv4' && !interfaceDetails.internal) {
+//           ipAddress = interfaceDetails.address;
+//           break;
+//         }
+//       }
+//       if (ipAddress) break;
+//     }
+//     console.log(`Servidor rodando em https://${ipAddress}:${port}`);
+// });
