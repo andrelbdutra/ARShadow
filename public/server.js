0e3483e987632458bb7d25a0e51a6d44a21ab0d9
andrelbdutra <bazulylbd@gmail.com>
2023-10-17 07:37:30 -0300

    adicionando ShadowGAN

--- public/server.js
+++ public/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const {PythonShell} = require('python-shell');
 const https = require('https');
 const cors = require('cors');
 const fs = require('fs');
@@ -14,6 +15,11 @@ app.use(express.static('public'));
 
 app.post('/upload', (req, res) => {
   const image = req.body.image;
+  //console.log(image)
+  PythonShell.run('public/test2.py').then(messages=>{
+    // results is an array consisting of messages collected during execution
+    console.log('results: %j', messages);
+ });
   // resposta de sucesso
   const processResult = {
       result: [-0.04780066504223751, 5.23935357881906, -3.1428021252333442],
@@ -24,30 +30,30 @@ app.post('/upload', (req, res) => {
     res.json(processResult);
   });
   
-  app.listen(process.env.PORT || 3000) // para deploy na railway
+  //app.listen(process.env.PORT || 3000) // para deploy na railway
   
-  // para executar localmente
-  // Certificado ssl (não válidado)
-//   const openSSL = {
-//     key: fs.readFileSync('./ssl/key.pem'),
-//     cert: fs.readFileSync('./ssl/cert.pem')
-//   };
-// const server = https.createServer(openSSL, app);
-// server.listen(port, () => {
-//     const os = require('os');
-//     const networkInterfaces = os.networkInterfaces();
-//     let ipAddress;
+  //para executar localmente
+  //Certificado ssl (não válidado)
+  const openSSL = {
+    key: fs.readFileSync('./ssl/key.pem'),
+    cert: fs.readFileSync('./ssl/cert.pem')
+  };
+const server = https.createServer(openSSL, app);
+server.listen(port, () => {
+    const os = require('os');
+    const networkInterfaces = os.networkInterfaces();
+    let ipAddress;
 
-//     // Encontre o endereço IP da máquina local (geralmente o primeiro na lista)
-//     for (const interfaceName in networkInterfaces) {
-//       const networkInterface = networkInterfaces[interfaceName];
-//       for (const interfaceDetails of networkInterface) {
-//         if (interfaceDetails.family === 'IPv4' && !interfaceDetails.internal) {
-//           ipAddress = interfaceDetails.address;
-//           break;
-//         }
-//       }
-//       if (ipAddress) break;
-//     }
-//     console.log(`Servidor rodando em https://${ipAddress}:${port}`);
-// });
+    // Encontre o endereço IP da máquina local (geralmente o primeiro na lista)
+    for (const interfaceName in networkInterfaces) {
+      const networkInterface = networkInterfaces[interfaceName];
+      for (const interfaceDetails of networkInterface) {
+        if (interfaceDetails.family === 'IPv4' && !interfaceDetails.internal) {
+          ipAddress = interfaceDetails.address;
+          break;
+        }
+      }
+      if (ipAddress) break;
+    }
+    console.log(`Servidor rodando em https://${ipAddress}:${port}`);
+});
